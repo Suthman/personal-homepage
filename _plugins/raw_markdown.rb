051fb94e754343930d2e8916f26cfc73a454b624
Suthman <chr.mi@web.de>
2026-07-28 19:31:02 +0200

    bugfix: style tags are not removed from md-files

--- _plugins/raw_markdown.rb
+++ _plugins/raw_markdown.rb
@@ -25,41 +25,36 @@ module Jekyll
       full_path = site.in_source_dir(doc.relative_path)
       return unless File.exist?(full_path)
 
-      file_content = File.read(full_path, encoding: 'utf-8')
+      file_content = File.read(full_path, encoding: "utf-8")
       content_parts = file_content.split(/^---+\s*$/)
-      
+
       if content_parts.size >= 3
         # Extract the raw markdown body (everything after the front matter)
         raw_markdown = content_parts[2..].join("---").strip
-        
-        # Cleanup HTML style blocks before rendering
-        raw_markdown.gsub!(/<style>.*?<\/style>/m, '')
 
         begin
           # Create a proper Liquid context using Jekyll's internal template environment
-          # This provides the necessary registers (like :site, :page) so {% include %} tags don't crash
-          context = Liquid::Context.new({}, {}, { :site => site, :page => doc.data })
+          context = Liquid::Context.new({}, {}, { site: site, page: doc.data })
           template = site.liquid_renderer.file(doc.path).parse(raw_markdown)
-          
-          # Render the liquid tags (includes will be resolved into their raw text/markdown/html output)
+
+          # Render the liquid tags
           rendered_markdown = template.render(context)
         rescue => e
           Jekyll.logger.warn "RawMarkdownGenerator:", "Could not render Liquid tags in #{doc.relative_path}: #{e.message}"
           rendered_markdown = raw_markdown
         end
 
-        # Post-rendering cleanup: Remove raw HTML tags that includes might have introduced (e.g., <p>, <br>),
-        # but keep normal text and markdown symbols like ## or - completely intact.
-        rendered_markdown.gsub!(/<\/?[^>]*>/, '')
+        # Removes HTML tags except <style> and </style> tags
+        rendered_markdown.gsub!(/<(?!style\b|\/style\b)\/?[^>]*>/i, "")
 
         # Hierarchy shift: Dynamically shift all markdown headers down based on the constant
-        rendered_markdown.gsub!(/^(#+)(?=\s)/) do |header_match| 
+        rendered_markdown.gsub!(/^(#+)(?=\s)/) do |header_match|
           header_match + ("#" * HEADER_SHIFT_LEVEL)
         end
-        
-        doc.data['raw_md'] = rendered_markdown.strip
+
+        doc.data["raw_md"] = rendered_markdown.strip
       else
-        doc.data['raw_md'] = doc.content
+        doc.data["raw_md"] = doc.content
       end
     end
   end
